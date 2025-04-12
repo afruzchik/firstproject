d64c55779edb62f00bb0bd728bd77e275bd75860
Afruzchik <shahrishaha15@example.com>
2025-04-12 16:38:59 +0300

    second_commit

--- movie/admin.py
+++ movie/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Movie
 
-admin.site.register(Movie)
+
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ("name", "year", "date")
+    list_filter = ("year", "date")
+    fields = ("name", "year", "date")
+
+
+admin.site.register(Movie, MovieAdmin)
